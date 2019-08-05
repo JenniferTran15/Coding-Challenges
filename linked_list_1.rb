2dfb621c35e16af9014fd267af7e418030b0e6fe
Jennifer Tran <jennifertran135@gmail.com>
2019-08-04 21:51:33 -0400

    LinkedList1

--- linked_list_1.rb
+++ linked_list_1.rb
@@ -7,7 +7,7 @@ class LinkedListNode
   end
 end
 
-class Staqck
+class Stack
   attr_reader :data
 
   def initialize
@@ -17,3 +17,35 @@ class Staqck
   def push(value)
     @data = LinkedListNode.new(value, @data)
   end
+
+  def pop
+    @data = @data.next_node
+  end
+end
+
+def reverse_list(list)
+  stack = Stack.new
+  while list != nil
+    stack.push(list.value)
+    list = list.next_node
+  end
+  return stack.data
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+puts "-------"
+revlist = reverse_list(node3)
+print_values(revlist)
